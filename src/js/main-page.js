0e75dc1efd1b84bd5c4aadebd6208890b41ae6ac
MuhabAlSadi <muhabalsadi23@gmail.com>
2024-07-01 10:51:45 +0300

    Update main-page.js

--- src/js/main-page.js
+++ src/js/main-page.js
@@ -1,16 +1,16 @@
 
 // - - - - - - - - - - - Main Functions to use - - - - - - - - - -
-/*
+
 // For buttons to transport from html page to another html page
 function transportPageButton(buttonClass, pageName){
 if(document.querySelector(`.${buttonClass}`)){
   document.querySelector(`.${buttonClass}`).addEventListener('click', function(){
-    window.location.href = `../pages/${pageName}.html`;
+    window.location.href = `./src/pages/${pageName}.html`;
   })
 }
 };
-*/
-// - - - - - - -- - -- - -- Header Section - -- - - - - - - - - - 
+
+// - - - - - - -- - -- - -- Header Section - -- - - - - - - - -  
 
 function showSiderbar(){
   const sidebar = document.querySelector('.sidebar')
@@ -23,35 +23,23 @@ function hideSidebar(){
 
 //Calculate Type Button in Header Bar
 document.querySelector('.calculate-button-header').addEventListener('click', function(){
-  window.location.href = './pages/calculating-type.html';
+  window.location.href = './src/pages/calculating-type.html';
 });
 
 
 // - - - - - - - - - - Calculating Button Section - - - - - - - - - - 
 
-//transportPageButton('calculate-button', 'calculating-type');
-document.getElementById('button').addEventListener('click', function(){
-    window.location.href = './pages/calculating-type.html';
-  })
+transportPageButton('calculate-button', 'calculating-type');
 
 // - - - - - - - - - Calculating Type Section - - - - - - - - - - - 
 
-//transportPageButton('final-result', 'calculating-the-exempt');
-//transportPageButton(`unknown-marks`, `calculate-required-grades`);
-
-document.querySelector(`.final-result`).addEventListener('click', function(){
-    window.location.href = `../pages/calculating-the-exempt.html`;
-  })
-document.querySelector(`.unknown-marks`).addEventListener('click', function(){
-    window.location.href = `./pages/calculate-required-grades.html`;
-  })
+transportPageButton('final-result', 'calculating-the-exempt');
+transportPageButton(`unknown-marks`, `calculate-required-grades`);
 
 // - - - - - - - - - - - Message Us Section -- - - - - - - - - -
 
-//transportPageButton(`calculate-button`, `calculating-type`);
-document.querySelector(`.mu-button`).addEventListener('click', function(){
-    window.location.href = `./pages/calculating-type.html`;
-  })
+transportPageButton(`mu-button`, `message-us`);
+
 // - - - - - - - - - - - - Social Media Links - - - - - - - - - - -
 document.getElementById('facebookImg').addEventListener('click', function(){
   window.open('https://www.facebook.com/people/Natigati-Website/61560080236721/', '_blank' );
